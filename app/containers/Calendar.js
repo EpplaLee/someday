eda4f0602beca2915b8815e5f23dc77fe457936d
EpplaeLee <350043743@qq.com>
2018-08-10 16:16:39 +0800

    first commmit

---
+++ app/containers/Calendar.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react'
+import { StyleSheet, View, Image } from 'react-native'
+import { connect } from 'react-redux'
+import { CalendarList, Agenda } from 'react-native-calendars';
+import Calendar from '../calendar';
+import { Button } from '../components'
+
+import { createAction, NavigationActions } from '../utils'
+
+@connect(({ app }) => ({ ...app }))
+class CalendarPage extends Component {
+  static navigationOptions = {
+    tabBarLabel: 'Calendar',
+    tabBarIcon: ({ focused, tintColor }) => (
+      <Image
+        style={[styles.icon, { tintColor: focused ? tintColor : 'gray' }]}
+        source={require('../images/person.png')}
+      />)
+  }
+
+  render() {
+    const { login } = this.props
+    return (
+      <View style={styles.container}>
+        <Calendar style={{
+          width: '100%',
+          height: '100%',
+        }} />
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  icon: {
+    width: 32,
+    height: 32,
+  },
+})
+
+export default CalendarPage
---
+++ app/containers/CalendarList.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react'
+import { StyleSheet, View, Image } from 'react-native'
+import { connect } from 'react-redux'
+import { Calendar, CalendarList, Agenda } from 'react-native-calendars';
+import { Button } from '../components'
+import { NavigationActions } from '../utils'
+
+@connect()
+class CalendarListPage extends Component {
+  static navigationOptions = {
+    tabBarLabel: 'CalendarList',
+    tabBarIcon: ({ focused, tintColor }) => (
+      <Image
+        style={[styles.icon, { tintColor: focused ? tintColor : 'gray' }]}
+        source={require('../images/house.png')}
+      />
+    ),
+  }
+
+
+  render() {
+    return (
+      <View style={styles.container}>
+       <CalendarList />
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  icon: {
+    width: 32,
+    height: 32,
+  },
+})
+
+export default CalendarListPage
